c78c3918af914553a0e2ab43fab3bc4f823d88c8
Mark <ratrateroo@gmail.com>
2020-02-26 22:52:47 +0800

    added body parser for handling json

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+//app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
 
 app.use('/feed', feedRoutes);
 
