787b0e3091ec8fcc176a529c34a1053af82f34f6
Mark <ratrateroo@gmail.com>
2020-02-29 23:31:41 +0800

    added the uploaded image for creating post

--- controllers/feed.js
+++ controllers/feed.js
@@ -23,12 +23,19 @@ exports.createPost = (req, res, next) => {
     throw error;
   }
 
+  if (!req.file) {
+    const error = new Error('No image provided.');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'image/1DX_MARKIII.png',
+    imageUrl: imageUrl,
     creator: {
       name: 'Mark',
     },
