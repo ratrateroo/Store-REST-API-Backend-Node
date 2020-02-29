1ebe11da161660d21b522eac352e3bd093c3a0a1
Mark <ratrateroo@gmail.com>
2020-02-29 22:42:45 +0800

    added error handling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -8,6 +9,8 @@ const app = express();
 //app.use(bodyParser.urlencoded());
 
 app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,6 +23,14 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
 const MONGODB_URI = 'mongodb://localhost/storerestdb';
 mongoose
   .connect(MONGODB_URI)
