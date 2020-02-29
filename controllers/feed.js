a5b61e70ca039a20841c79e0d9c9d19d200c6f67
Mark <ratrateroo@gmail.com>
2020-02-29 20:50:05 +0800

    added some dummy data for testing

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,9 +2,14 @@ exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
+        _id: '1',
         title: 'First Post',
         content: 'This is the first post!',
         imageUrl: 'image/1DX_MARKIII.png',
+        creator: {
+          name: 'Mark',
+        },
+        date: new Date(),
       },
     ],
   });
