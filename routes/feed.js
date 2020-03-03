5b3d2b48653052ba995da3374959f7b801b05c80
Mark <ratrateroo@gmail.com>
2020-03-03 23:19:55 +0800

    added auth middleware to all the routes

--- routes/feed.js
+++ routes/feed.js
@@ -11,6 +11,7 @@ router.get('/posts', isAuth, feedController.getPosts);
 
 router.post(
   '/post',
+  isAuth,
   [
     body('title')
       .trim()
@@ -22,10 +23,11 @@ router.post(
   feedController.createPost
 );
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 
 router.put(
   '/post/:postId',
+  isAuth,
   [
     body('title')
       .trim()
@@ -37,6 +39,6 @@ router.put(
   feedController.updatePost
 );
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
