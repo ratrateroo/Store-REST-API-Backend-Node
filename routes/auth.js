f58f879d9d6ae7899aaa064d412dcd5048a819e2
Mark <ratrateroo@gmail.com>
2020-03-03 18:34:47 +0800

    added validation for the email

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const { body } = require('express-validator');
+
+const User = require('../models/user');
 
 const router = express.Router();
 
-router.put('/signup');
+router.put('/signup', [
+  body('email')
+    .isEmail()
+    .withMessage('Please enter a valid email')
+    .custom((value, { req }) => {
+      return User.findOne({ email: value }).then(userDoc => {
+        if (userDoc) {
+          return Promise.reject('Email address already exists!');
+        }
+      });
+    }),
+]);
 
 module.exports = router;
