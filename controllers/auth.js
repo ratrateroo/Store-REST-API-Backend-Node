a36e53c15234cbc07f84ccb4879e34407ece0e3f
Mark <ratrateroo@gmail.com>
2020-03-03 18:47:27 +0800

    added signup controller with error handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,17 @@
 const User = require('../models/user');
+const { validationResult } = require('express-validator');
 
-exports.signup = 
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation Failed');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstname = req.body.firstname;
+  const middlename = req.body.middlename;
+  const lastname = req.body.lastname;
+};
--- routes/auth.js
+++ routes/auth.js
@@ -2,36 +2,41 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const User = require('../models/user');
+const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.put('/signup', [
-  body('email')
-    .isEmail()
-    .withMessage('Please enter a valid email')
-    .custom((value, { req }) => {
-      return User.findOne({ email: value }).then(userDoc => {
-        if (userDoc) {
-          return Promise.reject('Email address already exists!');
-        }
-      });
-    })
-    .normalizeEmail(),
-  body('password')
-    .trim()
-    .isLength({ min: 5 }),
-  body('firstname')
-    .trim()
-    .not()
-    .isEmpty(),
-  body('middlename')
-    .trim()
-    .not()
-    .isEmpty(),
-  body('lastname')
-    .trim()
-    .not()
-    .isEmpty(),
-]);
+router.put(
+  '/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject('Email address already exists!');
+          }
+        });
+      })
+      .normalizeEmail(),
+    body('password')
+      .trim()
+      .isLength({ min: 5 }),
+    body('firstname')
+      .trim()
+      .not()
+      .isEmpty(),
+    body('middlename')
+      .trim()
+      .not()
+      .isEmpty(),
+    body('lastname')
+      .trim()
+      .not()
+      .isEmpty(),
+  ],
+  authController.signup
+);
 
 module.exports = router;
