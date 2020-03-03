4d8d466ee82ee7179cd52b408b87ae68fb8317ba
Mark <ratrateroo@gmail.com>
2020-03-03 23:58:49 +0800

    added authorization

--- controllers/feed.js
+++ controllers/feed.js
@@ -130,6 +130,12 @@ exports.updatePost = (req, res, next) => {
         throw error;
       }
 
+      if (post.creator.toString() === req.userId) {
+        const error = new Error('Not authorized');
+        error.statusCode = 403;
+        throw error;
+      }
+
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
