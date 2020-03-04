6a84a557ba1cdac74561cacc06e6cd3f9524b82d
Mark <ratrateroo@gmail.com>
2020-03-04 19:33:06 +0800

    added logic for client connection notification

--- app.js
+++ app.js
@@ -71,5 +71,8 @@ mongoose
     console.log('Connected to Database!');
     const server = app.listen(8080);
     const io = require('socket.io')(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   })
   .catch(err => console.log(err));
