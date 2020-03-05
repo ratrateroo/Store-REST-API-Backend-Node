da8401b99c822966d1b9b5812fc909c66b09bf07
Mark <ratrateroo@gmail.com>
2020-03-05 21:56:08 +0800

    added shop route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 const authRoutes = require('./routes/auth');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
@@ -56,6 +57,7 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
+app.use('/shop', authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
